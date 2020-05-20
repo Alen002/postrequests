b76395d7c0c75ee8805b051e6f239fa66ec957b2
Alen <alechkr@gmail.com>
2020-05-20 15:54:30 +0200

    minor adjustments

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(morgan('short'));  //short or combines
 app.use(express.static('./public/')); //server is serving all the files inside the public folder
 
-app.use(bodyParser.urlencoded({extended: false})); // a piece of middleware to helps process request easier
+app.use(bodyParser.urlencoded({extended: false})); // middleware - necessary to receive the inform. from data entered in the form
 /* app.use(bodyParser.json()); */
 
 
@@ -37,28 +37,17 @@ app.get(('/users/:id'), (req, res) => {
  console.log(req.params);  //this shows you the param you entered in :id, /users/1 req.params is {id: '1'} 
  console.log(req.params.id);  
  res.send(users[req.params.id-1]);
-
 });
-
-
-/* app.post(('/post'), (req, res) => { 
-  const user = {
-    id: users.length + 1,
-    firstName: req.body.firstName,
-    lastName: req.body.lastName
-  };
-  users.push(user);
-  console.log(user);
-}); */
 
 app.post(('/user_create'), (req, res) => {
   console.log('Trying to create a new user:');
   const user = {
     id: users.length + 1,
-    firstName: req.body.first_name,
-    lastName: req.body.last_name
+    firstName: req.body.first_name, //taken from the form name="first_name"
+    lastName: req.body.last_name //taken from the form name="last_name"
   };
   users.push(user);
   console.log(user);
+  res.send('User updated on server');
   res.end();
 });
